e49c21aebf25a27fe2fe20f5f58941b70d889a13
grishagr9 <grisha-rnd10@yandex.ru>
2026-04-11 12:46:04 +0300

    Добавлена функция readListPeople, которая считывает файл people.txt

--- src/files/ParseFromPeople.java
+++ src/files/ParseFromPeople.java
@@ -1,7 +1,38 @@
 package files;
 
+import lesson_class.Person;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class ParseFromPeople {
     public static void main(String[] args) {
-        System.out.println("новый код");
+        readListPeople();
+    }
+
+    static void readListPeople() {
+        try {
+            File file = new File("src/files/people.txt");
+            Scanner scanner = new Scanner(file);
+            List<Person> people = new ArrayList<>();
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(" ");
+
+                String name = parts[0];
+                int age = Integer.parseInt(parts[1]);
+                String city = parts[2];
+
+                people.add(new Person(name, age, city));
+            }
+
+            System.out.println(people);
+
+        } catch (Exception e) {
+            System.out.println("Произошла ошибка чтения из файла " + e.getMessage());
+        }
     }
 }
