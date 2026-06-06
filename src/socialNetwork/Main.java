b3ff5481d82911e6138530b864bc19bc79bcac35
grishagr9 <grisha-rnd10@yandex.ru>
2026-06-06 12:07:00 +0300

    Программа

--- src/socialNetwork/Main.java
+++ src/socialNetwork/Main.java
@@ -2,21 +2,36 @@ package socialNetwork;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         UserStorage storage = new UserStorage();
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("""
+                    Нажмите кнопку, чтобы:
+                    1 - Добавить нового пользователя
+                    2 - Добавить друга
+                    3 - 
+                    4 - 
+                    5 - выход
+                    6 -""");
+            System.out.print("Введите команду ");
+            int knopka = scanner.nextInt();
+            if (knopka == 1) {
 
-        //Реализовать добавление друзей
-        //1. Создавть челдовека. Создать еще одного. Добавить их в друзья
+            } else if (knopka == 2) {
 
+            } else if (knopka == 3) {
+
+            } else if (knopka == 5) {
+                System.out.println("До свидания!");
+                break;
+            }
+    }
 
         storage.save();
-        List<Person> personList = storage.getPersons();
-        System.out.println("Список всех пользователей:");
-        for (Person p : personList) {
-            System.out.println(p);
-        }
     }
 }
