f9ffb58078e341889c2b9d606db525affc72195e
grishagr9 <grisha-rnd10@yandex.ru>
2026-06-13 12:08:16 +0300

    спрятал реализацию в функции

--- src/socialNetwork/Main.java
+++ src/socialNetwork/Main.java
@@ -10,43 +10,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("""
-                    Нажмите кнопку, чтобы:
-                    1 - Добавить нового пользователя
-                    2 - Вывод всех пользователей на экран
-                    3 - Добавить друга
-                    4 - 
-                    5 - выход
-                    6 -""");
-            System.out.print("Введите команду ");
+            printMenu();
             int knopka = scanner.nextInt();
             if (knopka == 1) {
-                System.out.print("Введите логин: ");
-                String login = scanner.next();
-                System.out.print("Введите имя: ");
-                String firstName = scanner.next();
-                System.out.print("Введите фамилию: ");
-                String lastName = scanner.next();
-                System.out.print("Введите город: ");
-                String city = scanner.next();
-                System.out.print("Введите дату рождения введите год ");
-                int year = scanner.nextInt();
-                System.out.print("Введите дату рождения введите месяц ");
-                int month = scanner.nextInt();
-                System.out.print("Введите дату рождения введите день ");
-                int day = scanner.nextInt();
-
-                Person newPerson = new Person(login, firstName, lastName, city, LocalDate.of(year, month , day));
-                storage.savePerson(newPerson);
-                System.out.println("Пользователь добавлен.айди пользваотеля: " + newPerson.getId());
+                registration(scanner, storage);
             } else if (knopka == 2) {
-                List<Person> personlist = storage.getPersons();
-                for (Person person : personlist) {
-                    System.out.println(person.getLogin() +
-                            ": " + person.getFirstName() + " " +
-                            person.getLastName() + " город " +
-                            person.getCity());
-                }
+               printUsers(storage);
             } else if (knopka == 3) {
                 //введите логин кого хотите добавить
 
@@ -58,4 +27,48 @@ public class Main {
 
         storage.save();
     }
+
+    static void printUsers(UserStorage storage) {
+        List<Person> personlist = storage.getPersons();
+        for (Person person : personlist) {
+            System.out.println(person.getLogin() +
+                    ": " + person.getFirstName() + " " +
+                    person.getLastName() + " город " +
+                    person.getCity());
+        }
+    }
+
+    static void printMenu(){
+        System.out.println("""
+                    Нажмите кнопку, чтобы:
+                    1 - Добавить нового пользователя
+                    2 - Вывод всех пользователей на экран
+                    3 - Добавить друга
+                    4 - 
+                    5 - выход
+                    6 -""");
+        System.out.print("Введите команду ");
+    }
+
+    static void registration(Scanner scanner, UserStorage storage) {
+        System.out.print("Введите логин: ");
+        String login = scanner.next();
+        System.out.print("Введите имя: ");
+        String firstName = scanner.next();
+        System.out.print("Введите фамилию: ");
+        String lastName = scanner.next();
+        System.out.print("Введите город: ");
+        String city = scanner.next();
+        System.out.print("Введите дату рождения введите год ");
+        int year = scanner.nextInt();
+        System.out.print("Введите дату рождения введите месяц ");
+        int month = scanner.nextInt();
+        System.out.print("Введите дату рождения введите день ");
+        int day = scanner.nextInt();
+
+        Person newPerson = new Person(login, firstName, lastName, city, LocalDate.of(year, month , day));
+        storage.savePerson(newPerson);
+        System.out.println("Пользователь добавлен.айди пользваотеля: " + newPerson.getId());
+    }
+
 }
